378ee1232c5db153133af48594e4668ee7cf203a
lizp <13699836954@139.com>
2016-07-17 22:19:41 +0800

    初始化

---
+++ gitRespository/gitRespository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitRespository
+//
+//  Created by lizp on 16/7/17.
+//  Copyright © 2016年 LZP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitRespository/gitRespository/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitRespository
+//
+//  Created by lizp on 16/7/17.
+//  Copyright © 2016年 LZP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
